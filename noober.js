65f58525c51179f144a76427ccbfcf050192f581
S Reddy <sachitgreddy@email.com>
2021-01-28 07:56:20 -0500

    Done, made minor changes to previous

--- noober.js
+++ noober.js
@@ -8,17 +8,33 @@ function pageLoad() {
   // e.g. ask 'ride' for its length or only grab the first member of the Array
   console.log(ride)
 
+  // levelOfService
+  // Sets the title of the page
+
   if (ride.length > 1 || ride[0].numberofPassengers == 1) {
-    levelOfService = 'Noober Pool'      //Single riders can be part of Noober pool
+    levelOfService = 'HW3 Noober Pool'      //Single riders can be part of Noober pool
   } else if (ride[0].purpleRequested == true) {
-    levelOfService = 'Noober Purple'    //If the passenger requests level of service as purple
+    levelOfService = 'HW3 Noober Purple'    //If the passenger requests level of service as purple
   } else if (ride[0].numberOfPassengers > 3) {
-    levelOfService = 'Noober XL'        //If the ride opts for a larger car with more than 3 passengers
-  } else {levelOfService = 'Noober X'}  //This is the normal level of service without any exceptions
+    levelOfService = 'HW3 Noober XL'        //If the ride opts for a larger car with more than 3 passengers
+  } else {levelOfService = 'HW3 Noober X'}  //This is the normal level of service without any exceptions
 
   console.log(levelOfService)
 
-  //Now we start setting the passenger or ride details such as Name, Phone, Pickup & Dropoff location and #ofpassengers by ride
+  // these variables map to the elements on the finished page;
+  // assign the proper value to each variable
+  
+  // passenger1Name, passenger1Phone, passenger1NumberOfPassengers (went with rubric instructions and put this at last)
+  // passenger1PickupAddressLine1, passenger1PickupAddressLine2
+  // passenger1DropoffAddressLine1, passenger1DropoffAddressLine2
+  // passenger2Name, passenger2Phone, passenger2NumberOfPassengers
+  // passenger2PickupAddressLine1, passenger2PickupAddressLine2
+  // passenger2DropoffAddressLine1, passenger2DropoffAddressLine2
+  // passenger3Name, passenger3Phone, passenger3NumberOfPassengers
+  // passenger3PickupAddressLine1, passenger3PickupAddressLine2
+  // passenger3DropoffAddressLine1, passenger3DropoffAddressLine2
+
+  // Now we start setting the passenger or ride details such as Name, Phone, Pickup & Dropoff location and #ofpassengers by ride
 
   if (ride.length == 1){
     passenger1Name = ride[0].passengerDetails.first + ' ' + ride[0].passengerDetails.last
@@ -114,20 +130,6 @@ function pageLoad() {
     console.log(passenger1NumberOfPassengers)
   }
 
-  // these variables map to the elements on the finished page;
-  // assign the proper value to each variable
-
-  // levelOfService
-  //Sets the title of the page
-  // passenger1Name, passenger1Phone, passenger1NumberOfPassengers
-  // passenger1PickupAddressLine1, passenger1PickupAddressLine2
-  // passenger1DropoffAddressLine1, passenger1DropoffAddressLine2
-  // passenger2Name, passenger2Phone, passenger2NumberOfPassengers
-  // passenger2PickupAddressLine1, passenger2PickupAddressLine2
-  // passenger2DropoffAddressLine1, passenger2DropoffAddressLine2
-  // passenger3Name, passenger3Phone, passenger3NumberOfPassengers
-  // passenger3PickupAddressLine1, passenger3PickupAddressLine2
-  // passenger3DropoffAddressLine1, passenger3DropoffAddressLine2
-  
+    
   //  👆 YOUR CODE ENDS HERE. DON'T CHANGE ANY OTHER CODE 👆
 }
