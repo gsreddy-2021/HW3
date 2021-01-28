526597bfd1b2e700cd9899ede8c72f837dd8dd7a
S Reddy <sachitgreddy@email.com>
2021-01-28 05:24:32 -0500

    Added the if rules to define the levelsofservice

--- noober.js
+++ noober.js
@@ -8,11 +8,20 @@ function pageLoad() {
   // e.g. ask 'ride' for its length or only grab the first member of the Array
   console.log(ride)
 
+  if (ride.length > 1 || ride[0].numberofPassengers == 1) {
+    levelOfService = 'Noober Pool'      //Single riders can be part of Noober pool
+  } else if (ride[0].purpleRequested == true) {
+    levelOfService = 'Noober Purple'    //If the passenger requests level of service as purple
+  } else if (ride[0].numberOfPassengers > 3) {
+    levelOfService = 'Noober XL'        //If the ride opts for a larger car with more than 3 passengers
+  } else {levelOfService = 'Noober X'}  //This is the normal level of service without any exceptions
+
+
   // these variables map to the elements on the finished page;
   // assign the proper value to each variable
 
   // levelOfService
-  levelOfService = 'tacos'  //Sets the title of the page
+  //Sets the title of the page
   // passenger1Name, passenger1Phone, passenger1NumberOfPassengers
   // passenger1PickupAddressLine1, passenger1PickupAddressLine2
   // passenger1DropoffAddressLine1, passenger1DropoffAddressLine2
